2075905a3f5f9a59300f581d655c7e4bbc9025f4
Scott <kieran.scott@siemens.com>
2024-09-03 15:44:34 +0100

    added ability to cycle through weapon(projectile) sizes

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.timer = 0
+        self.ammo_type = "normal"
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -24,23 +25,30 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.move(dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.move(-dt)
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
         if keys[pygame.K_SPACE]:
             if self.timer <= 0:
                 self.shoot()
+        if keys[pygame.K_1]:
+            self.cycle_ammo_type(0)
+        if keys[pygame.K_2]:
+            self.cycle_ammo_type(1)
+        if keys[pygame.K_3]:
+            self.cycle_ammo_type(2)
+        if keys[pygame.K_4]:
+            self.cycle_ammo_type(3)
 
         self.timer -= dt
         
-
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
+        shot = Shot(self.position.x, self.position.y, self.ammo_type)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
         self.timer = PLAYER_SHOT_COOLDOWN
 
@@ -50,3 +58,6 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def cycle_ammo_type(self, ammo_index):
+        self.ammo_type = CYCLABLE_AMMO_TYPES[ammo_index]
--- shot.py
+++ shot.py
@@ -3,9 +3,22 @@ from circleshape import CircleShape
 from constants import *
 
 class Shot(CircleShape):
-    def __init__(self, x, y):
-        super().__init__(x, y, SHOT_RADIUS)
+    def __init__(self, x, y, ammo_type):
+        super().__init__(x, y, self.get_shot_radius(ammo_type))
     
+    def get_shot_radius(self, ammo_type):
+        match ammo_type:
+            case "normal":
+                return NORMAL_SHOT_RADIUS
+            case "big":
+                return BIG_SHOT_RADIUS
+            case "bigger":
+                return BIGGER_SHOT_RADIUS
+            case "cheating":
+                return CHEATING_SHOT_RADIUS
+            
+        return NORMAL_SHOT_RADIUS
+
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius)
 
