371e5b9438a4c28cf55320e203c796391d53b658
Scott <kieran.scott@siemens.com>
2024-09-03 09:42:48 +0100

    lesson 6 complete

--- main.py
+++ main.py
@@ -2,22 +2,28 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
-    print("Starting asteroids!")
     pygame.init()
-    print(SCREEN_HEIGHT)
-    print(SCREEN_WIDTH)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    starting_player_x = SCREEN_WIDTH / 2
-    starting_player_y = SCREEN_HEIGHT / 2
-    player = Player(starting_player_x, starting_player_y)
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                 return
-        screen.fill((0,0,0))
+                return
+
+        player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
